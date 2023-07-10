ebb483a52e71faf7cb670e2629ffe48ce3bb0489
Daniel Roberto <danrob992010@hotmail.com>
2023-07-10 10:19:45 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const formulario = document.getElementById('meuFormulario');
+const mensagemDiv = document.getElementById('mensagem');
+formulario.addEventListener('submit', function(event) {
+  const campoA = parseInt(document.getElementById('campoA').value);
+  const campoB = parseInt(document.getElementById('campoB').value);
+  
+  if (campoB <= campoA) {
+    event.preventDefault(); // Impede a submissão do formulário
+    mensagemDiv.textContent = 'O número B deve ser maior que o número A!';
+    mensagemDiv.style.color = 'red';
+  } else {
+    mensagemDiv.textContent = 'Formulário válido!';
+    mensagemDiv.style.color = 'green';
+  }
+});
